cdce342368fb367a34ca8c8f3666efddb3ac3f05
Samuel Griff <sgriff@zohomail.com>
2022-09-21 05:42:28 -0400

    Update testing

--- src/test/java/bernstein/controller/ApplicationControllerTest.java
+++ src/test/java/bernstein/controller/ApplicationControllerTest.java
@@ -2,10 +2,14 @@ package bernstein.controller;
 
 import bernstein.service.ApplicationService;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @WebMvcTest(ApplicationController.class)
 public class ApplicationControllerTest {
@@ -19,5 +23,12 @@ public class ApplicationControllerTest {
     @Test
     public void shouldWork1() throws Exception {
         webClient.getPage("/");
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldWork2() throws Exception {
+        HtmlPage page = webClient.getPage("/deployments");
+        assertThat(page.getTitleText()).isEqualTo("Deployments");
     }
 }
